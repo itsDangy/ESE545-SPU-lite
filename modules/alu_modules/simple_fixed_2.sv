9a35532019849c6c0c279b744cc1b60563b4f750
itsDangy <dang.william02@gmail.com>
2025-03-12 01:26:23 -0400

    simple fixed 2 finished (maybe - need to test) and resigter file is changed to BigEndian

--- modules/alu_modules/simple_fixed_2.sv
+++ modules/alu_modules/simple_fixed_2.sv
@@ -1,5 +1,76 @@
-module SimpleFixed2 (
-    ports
-);
+module Simple_Fixed_2 #(
+        localparam WIDTH=128,
+    )(
+
+        input [0:31] instruction,
+        input [0:WIDTH-1] RA_data_in,
+        input [0:WIDTH-1] RB_data_in,
+        input [0:WIDTH-1] RC_data_in,
+        output [0:WIDTH-1] RT_data_out,
+
+    );
+
+    logic [0:6] Immediate7;
+    assign Immediate7 = instruction[11:17];
+    
+    logic [0:31] s;
+    logic [0:31] t;
+    logic [0:31] r;
+
+    always_comb begin
+        if (instruction[4:7] == 4'b1111) begin
+            //Rotate Word Immediate - roti (RI7)
+            s := {25{Immediate7[0]},Immediate7} & 32'h0000001F;
+
+            for (j = 0; j < 15 ; j = j + 4 ) begin
+                t := RA_data_in[32*j:32*j+31];
+
+                for (b = 0; b < 31 ; b = b + 1 ) begin
+                    if (b + s < 32) begin
+                        r[b] := t[b+s];
+                    end else begin
+                        r[b] := t[b+s-32];
+                    end
+                end
+                rt_data_out[32*j:32*j+31] = r;
+            end
+
+        end else if (instruction[4:7] == 4'b1011) begin
+            if (instruction[8:10] == 3'b000) begin
+                //Rotate Word - rot (RR)
+                for (j = 0; j < 15 ; j = j + 4 ) begin
+                    s := RB_data_in[32*j:32*j+31] & 32'h0000001F;
+                    t := RA_data_in[32*j:32*j+31];
+
+                    for (b = 0; b < 31 ; b = b + 1 ) begin
+                        if (b + s < 32) begin
+                            r[b] := t[b+s];
+                        end else begin
+                            r[b] := t[b+s-32];
+                        end
+                    end
+                    rt_data_out[32*j:32*j+31] = r;
+                end
+
+            end else if (instruction[8:10] == 3'b011) begin
+                //Shift Left Halfword - shl (RR)
+                for (j = 0; j < 15 ; j = j + 4 ) begin
+                    s := RB_data_in[32*j:32*j+31] & 32'h0000003F;
+                    t := RA_data_in[32*j:32*j+31];
+
+                    for (b = 0; b < 31 ; b = b + 1 ) begin
+                        if (b + s < 32) begin
+                            r[b] := t[b+s];
+                        end else begin
+                            r[b] := 1'b0;
+                        end
+                    end
+                    rt_data_out[32*j:32*j+31] = r;
+                end
+            end
+        end else begin
+            //do nothing
+        end
+    end
     
 endmodule
--- modules/register_file.sv
+++ modules/register_file.sv
@@ -3,25 +3,25 @@ module register_file #(
         parameter WIDTH=128, SIZE=128,
         localparam LOGSIZE=$clog2(SIZE)
     )(
-        input [WIDTH-1:0] rt_even_data_in,
-        input [WIDTH-1:0] rt_odd_data_in,
-        output logic [WIDTH-1:0] ra_even_data_out,
-        output logic [WIDTH-1:0] rb_even_data_out,
-        output logic [WIDTH-1:0] rc_even_data_out,
-        output logic [WIDTH-1:0] ra_odd_data_out,
-        output logic [WIDTH-1:0] rb_odd_data_out,
-        output logic [WIDTH-1:0] rc_odd_data_out,
-        input [LOGSIZE-1:0] ra_even_addr_in,
-        input [LOGSIZE-1:0] rb_even_addr_in,
-        input [LOGSIZE-1:0] rc_even_addr_in,
-        input [LOGSIZE-1:0] rt_even_addr_in,
-        input [LOGSIZE-1:0] ra_odd_addr_in,
-        input [LOGSIZE-1:0] rb_odd_addr_in,
-        input [LOGSIZE-1:0] rc_odd_addr_in,
-        input [LOGSIZE-1:0] rt_odd_addr_in,
+        input [0:WIDTH-1] rt_even_data_in,
+        input [0:WIDTH-1] rt_odd_data_in,
+        output logic [0:WIDTH-1] ra_even_data_out,
+        output logic [0:WIDTH-1] rb_even_data_out,
+        output logic [0:WIDTH-1] rc_even_data_out,
+        output logic [0:WIDTH-1] ra_odd_data_out,
+        output logic [0:WIDTH-1] rb_odd_data_out,
+        output logic [0:WIDTH-1] rc_odd_data_out,
+        input [0:LOGSIZE-1] ra_even_addr_in,
+        input [0:LOGSIZE-1] rb_even_addr_in,
+        input [0:LOGSIZE-1] rc_even_addr_in,
+        input [0:LOGSIZE-1] rt_even_addr_in,
+        input [0:LOGSIZE-1] ra_odd_addr_in,
+        input [0:LOGSIZE-1] rb_odd_addr_in,
+        input [0:LOGSIZE-1] rc_odd_addr_in,
+        input [0:LOGSIZE-1] rt_odd_addr_in,
         input wr_en_even, wr_en_odd,
     );
-    logic [SIZE-1:0][WIDTH-1:0] mem;
+    logic [0-SIZE-1][0:WIDTH-1] mem;
 
     always_comb begin
         ra_even_data_out <= mem[ra_even_addr_in];
